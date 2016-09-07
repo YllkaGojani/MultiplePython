f0532916693557b074f8b207a6fc225d1bc7f0a1
YllkaGojani <yllka.gojani@gmail.com>
2016-09-07 21:56:15 +0200

    Added the file for the Multiple Python assignment.

---
+++ MultiplesPython.py
@@ -0,0 +1,14 @@
+def odd1To1k():
+	for i in range(1, 1000):
+		if(i % 2 == 1):
+			print i
+
+odd1To1k()
+
+
+def multiplesOf5():
+	for i in range(5, 1000000):
+		if(i % 5 == 0):
+			print i
+
+multiplesOf5()	
